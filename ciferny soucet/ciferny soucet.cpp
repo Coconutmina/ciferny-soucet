3809a19eeb99945e877c4f2bad1238d52aed9dec
matěj <matj@DESKTOP-1HI5AKA>
2022-03-03 18:11:36 +0100

    Přidejte soubory projektu.

---
+++ ciferny soucet/ciferny soucet.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+//ve škole mi nefungoval počítač tak nemám zbytek funkcí ale tato funkce by po dosazení snad měla vypočítat ciferný součet
+int cif_soucet(int x) {
+	int m, vysledek = 0;
+	while (x > 0) {
+		m = x % 10;
+		vysledek = vysledek + m;
+		x = x / 10;
+	}
+	return vysledek;
+}
+{
+	printf("cif soucet je %d", cif_soucet(23));
+}
